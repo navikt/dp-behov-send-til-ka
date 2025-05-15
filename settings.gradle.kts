2b252dd8c16e53600f78dea5d72740f8b3269d67
Kent Daleng <kent.daleng@nav.no>
2025-05-15 09:32:59 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "dp-SERVICENAME"
+dependencyResolutionManagement {
+    repositories {
+        maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    }
+    versionCatalogs {
+        create("libs") {
+            from("no.nav.dagpenger:dp-version-catalog:20250512.164.dbb9e2")
+        }
+    }
+}
